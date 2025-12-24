6f73a4bc90f17c1d409869e65d1564d89115b674
wangjun <578382239@qq.com>
2025-12-24 22:21:38 +0800

    用户主题选择

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
+import { useThemeStore } from './stores/theme'
 
 const app = createApp(App)
 const pinia = createPinia()
+
+useThemeStore(pinia).init()
 
 app.use(pinia)
 app.use(router)
