2efb2552c68f5818875593730d2dfba6f9db9aab
wangjun <578382239@qq.com>
2025-12-26 11:45:47 +0800

    side样式调整

--- frontend/src/components/layout/BrutalSidebar.vue
+++ frontend/src/components/layout/BrutalSidebar.vue
@@ -7,6 +7,7 @@ import BrutalModal from "../ui/BrutalModal.vue";
 import BrutalFormItem from "../ui/BrutalFormItem.vue";
 import BrutalRadio from "../ui/BrutalRadio.vue";
 import BrutalButton from "../ui/BrutalButton.vue";
+import Tooltip from "../ui/Tooltip.vue";
 
 const props = defineProps({
   collapsed: { type: Boolean, default: false },
@@ -112,165 +113,191 @@ const logoLetters = computed(() => logoText.split(""));
             v-for="(letter, index) in logoLetters"
             :key="`${letter}-${index}`"
             class="logo-letter"
-            >{{ letter }}</span>
+            >{{ letter }}</span
+          >
         </span>
       </div>
     </div>
 
     <nav class="sidebar-nav">
-      <button
+      <Tooltip
         v-for="item in menuItems"
         :key="item.key"
-        class="nav-item"
-        :class="{ active: isActive(item.path) }"
-        :title="collapsed ? item.label : ''"
-        @click="navigate(item.path)"
+        :content="item.label"
+        :disabled="!collapsed"
+        :as-child="true"
+        side="right"
       >
-        <span class="nav-icon">
-          <svg
-            v-if="item.icon === 'upload'"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z" />
-          </svg>
-          <svg
-            v-else-if="item.icon === 'folder'"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path
-              d="M10 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"
-            />
-          </svg>
-          <svg
-            v-else-if="item.icon === 'chart'"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path
-              d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z"
-            />
-          </svg>
-          <svg
-            v-else-if="item.icon === 'users'"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path
-              d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"
-            />
-          </svg>
-          <svg
-            v-else-if="item.icon === 'audit'"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path
-              d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"
-            />
-          </svg>
-          <svg
-            v-else-if="item.icon === 'settings'"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path
-              d="M19.14 12.94c.04-.31.06-.63.06-.94 0-.31-.02-.63-.06-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.04.31-.06.63-.06.94s.02.63.06.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z"
-            />
-          </svg>
-        </span>
-        <span v-show="!collapsed" class="nav-label">{{ item.label }}</span>
-      </button>
+        <button
+          class="nav-item"
+          :class="{ active: isActive(item.path) }"
+          @click="navigate(item.path)"
+        >
+          <span class="nav-icon">
+            <svg
+              v-if="item.icon === 'upload'"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z" />
+            </svg>
+            <svg
+              v-else-if="item.icon === 'folder'"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                d="M10 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"
+              />
+            </svg>
+            <svg
+              v-else-if="item.icon === 'chart'"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z"
+              />
+            </svg>
+            <svg
+              v-else-if="item.icon === 'users'"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"
+              />
+            </svg>
+            <svg
+              v-else-if="item.icon === 'audit'"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"
+              />
+            </svg>
+            <svg
+              v-else-if="item.icon === 'settings'"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                d="M19.14 12.94c.04-.31.06-.63.06-.94 0-.31-.02-.63-.06-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.04.31-.06.63-.06.94s.02.63.06.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z"
+              />
+            </svg>
+          </span>
+          <span v-show="!collapsed" class="nav-label">{{ item.label }}</span>
+        </button>
+      </Tooltip>
     </nav>
 
     <div class="sidebar-footer">
       <div class="user-section">
-        <div
-          class="user-info"
-          :title="collapsed ? authStore.user?.username : ''"
+        <Tooltip
+          :content="authStore.user?.username || 'User'"
+          :disabled="!collapsed"
+          :as-child="true"
+          side="right"
         >
-          <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-            <path
-              d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
-            />
-          </svg>
-          <span v-show="!collapsed" class="username">{{
-            authStore.user?.username || "User"
-          }}</span>
-        </div>
-        <button class="logout-btn" @click="handleLogout" title="退出登录">
-          <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-            <path
-              d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"
-            />
-          </svg>
-          <span v-show="!collapsed">退出</span>
-        </button>
-
-        <button
-          class="logout-btn theme-btn"
-          type="button"
-          :title="themeTitle"
-          :aria-label="themeTitle"
-          :aria-pressed="themeStore.isDark"
-          @click="handleToggleTheme"
-        >
-          <svg
-            v-if="themeStore.isDark"
-            viewBox="0 0 24 24"
-            width="18"
-            height="18"
-            fill="currentColor"
+          <div class="user-info">
+            <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+              <path
+                d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
+              />
+            </svg>
+            <span v-show="!collapsed" class="username">{{
+              authStore.user?.username || "User"
+            }}</span>
+          </div>
+        </Tooltip>
+        <Tooltip content="退出登录" :as-child="true" side="right">
+          <button
+            class="logout-btn"
+            type="button"
+            @click="handleLogout"
+            aria-label="退出登录"
           >
-            <path
-              d="M21.64 13.65A9 9 0 0 1 10.35 2.36a.75.75 0 0 0-.93-.93A10.5 10.5 0 1 0 22.57 14.58a.75.75 0 0 0-.93-.93z"
-            />
-          </svg>
-          <svg
-            v-else
-            viewBox="0 0 24 24"
-            width="18"
-            height="18"
-            fill="currentColor"
-          >
-            <path
-              d="M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.8 1.42-1.42zm10.45 0l1.79-1.8-1.41-1.41-1.8 1.79 1.42 1.42zM12 4V1h-1.99v3H12zm0 19v-3h-1.99v3H12zM4 13H1v-2h3v2zm19 0h-3v-2h3v2zM6.76 19.16l-1.79 1.8 1.41 1.41 1.8-1.79-1.42-1.42zm10.45 0l1.42 1.42 1.8 1.79 1.41-1.41-1.79-1.8-1.42 1.42zM12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12z"
-            />
-          </svg>
-          <span v-show="!collapsed">{{ themeLabel }}</span>
-        </button>
+            <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+              <path
+                d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"
+              />
+            </svg>
+            <span v-show="!collapsed">退出</span>
+          </button>
+        </Tooltip>
 
-        <button
-          class="logout-btn theme-type-btn"
-          type="button"
-          :title="uiThemeTitle"
-          :aria-label="uiThemeTitle"
-          @click="openUiThemeModal"
-        >
-          <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-            <path
-              d="M12 22c-5.52 0-10-4.48-10-10S6.48 2 12 2c5.52 0 10 4.48 10 10 0 1.1-.9 2-2 2h-1.5c-.83 0-1.5.67-1.5 1.5 0 .41.17.79.44 1.06.27.27.44.65.44 1.06 0 .83-.67 1.5-1.5 1.5H12zm0-18c-4.41 0-8 3.59-8 8s3.59 8 8 8h4c.28 0 .5-.22.5-.5 0-.13-.05-.26-.15-.35-.64-.64-1-1.51-1-2.41 0-1.93 1.57-3.5 3.5-3.5H20c.55 0 1-.45 1-1 0-4.41-3.59-8-9-8zM6.5 11.5c.83 0 1.5-.67 1.5-1.5S7.33 8.5 6.5 8.5 5 9.17 5 10s.67 1.5 1.5 1.5zm3-4c.83 0 1.5-.67 1.5-1.5S10.33 4.5 9.5 4.5 8 5.17 8 6s.67 1.5 1.5 1.5zm5 0c.83 0 1.5-.67 1.5-1.5S15.33 4.5 14.5 4.5 13 5.17 13 6s.67 1.5 1.5 1.5zm3 4c.83 0 1.5-.67 1.5-1.5S18.33 8.5 17.5 8.5 16 9.17 16 10s.67 1.5 1.5 1.5z"
-            />
-          </svg>
-          <span v-show="!collapsed">主题</span>
-        </button>
+        <Tooltip :content="themeTitle" :as-child="true" side="right">
+          <button
+            class="logout-btn theme-btn"
+            type="button"
+            :aria-label="themeTitle"
+            :aria-pressed="themeStore.isDark"
+            @click="handleToggleTheme"
+          >
+            <svg
+              v-if="themeStore.isDark"
+              viewBox="0 0 24 24"
+              width="18"
+              height="18"
+              fill="currentColor"
+            >
+              <path
+                d="M21.64 13.65A9 9 0 0 1 10.35 2.36a.75.75 0 0 0-.93-.93A10.5 10.5 0 1 0 22.57 14.58a.75.75 0 0 0-.93-.93z"
+              />
+            </svg>
+            <svg
+              v-else
+              viewBox="0 0 24 24"
+              width="18"
+              height="18"
+              fill="currentColor"
+            >
+              <path
+                d="M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.8 1.42-1.42zm10.45 0l1.79-1.8-1.41-1.41-1.8 1.79 1.42 1.42zM12 4V1h-1.99v3H12zm0 19v-3h-1.99v3H12zM4 13H1v-2h3v2zm19 0h-3v-2h3v2zM6.76 19.16l-1.79 1.8 1.41 1.41 1.8-1.79-1.42-1.42zm10.45 0l1.42 1.42 1.8 1.79 1.41-1.41-1.79-1.8-1.42 1.42zM12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12z"
+              />
+            </svg>
+            <span v-show="!collapsed">{{ themeLabel }}</span>
+          </button>
+        </Tooltip>
+
+        <Tooltip :content="uiThemeTitle" :as-child="true" side="right">
+          <button
+            class="logout-btn theme-type-btn"
+            type="button"
+            :aria-label="uiThemeTitle"
+            @click="openUiThemeModal"
+          >
+            <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+              <path
+                d="M12 22c-5.52 0-10-4.48-10-10S6.48 2 12 2c5.52 0 10 4.48 10 10 0 1.1-.9 2-2 2h-1.5c-.83 0-1.5.67-1.5 1.5 0 .41.17.79.44 1.06.27.27.44.65.44 1.06 0 .83-.67 1.5-1.5 1.5H12zm0-18c-4.41 0-8 3.59-8 8s3.59 8 8 8h4c.28 0 .5-.22.5-.5 0-.13-.05-.26-.15-.35-.64-.64-1-1.51-1-2.41 0-1.93 1.57-3.5 3.5-3.5H20c.55 0 1-.45 1-1 0-4.41-3.59-8-9-8zM6.5 11.5c.83 0 1.5-.67 1.5-1.5S7.33 8.5 6.5 8.5 5 9.17 5 10s.67 1.5 1.5 1.5zm3-4c.83 0 1.5-.67 1.5-1.5S10.33 4.5 9.5 4.5 8 5.17 8 6s.67 1.5 1.5 1.5zm5 0c.83 0 1.5-.67 1.5-1.5S15.33 4.5 14.5 4.5 13 5.17 13 6s.67 1.5 1.5 1.5zm3 4c.83 0 1.5-.67 1.5-1.5S18.33 8.5 17.5 8.5 16 9.17 16 10s.67 1.5 1.5 1.5z"
+              />
+            </svg>
+            <span v-show="!collapsed">主题</span>
+          </button>
+        </Tooltip>
       </div>
-      <button
-        class="collapse-btn"
-        @click="toggleCollapse"
-        :title="collapsed ? '展开' : '收起'"
+      <Tooltip
+        :content="collapsed ? '展开' : '收起'"
+        :as-child="true"
+        side="right"
       >
-        <svg
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          :class="{ rotated: collapsed }"
-        >
-          <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
-        </svg>
-      </button>
+        <button class="collapse-btn" type="button" @click="toggleCollapse">
+          <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            :class="{ rotated: collapsed }"
+          >
+            <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+          </svg>
+        </button>
+      </Tooltip>
 
-      <BrutalModal v-model:show="uiThemeModalVisible" title="主题" width="520px">
+      <BrutalModal
+        v-model:show="uiThemeModalVisible"
+        title="主题"
+        width="520px"
+      >
         <BrutalFormItem label="选择主题">
           <BrutalRadio
             v-model="uiThemeDraft"
@@ -340,8 +367,8 @@ const logoLetters = computed(() => logoText.split(""));
 }
 
 .logo-text {
-  font-family: var(--nb-font-mono);
-  font-weight: 900;
+  font-family: var(--nb-font-ui, var(--nb-font-mono));
+  font-weight: var(--nb-ui-font-weight-strong, 900);
   font-size: 22px;
   text-transform: uppercase;
   display: inline-flex;
@@ -359,8 +386,7 @@ const logoLetters = computed(() => logoText.split(""));
 
 @supports (-webkit-background-clip: text) {
   .logo-letter {
-    background-image:
-      linear-gradient(
+    background-image: linear-gradient(
         180deg,
         rgba(255, 255, 255, 0.55),
         rgba(255, 255, 255, 0)
@@ -374,31 +400,31 @@ const logoLetters = computed(() => logoText.split(""));
 }
 
 .logo-letter:nth-child(7n + 1) {
-  --logo-color: var(--nb-salmon);
+  --logo-color: var(--nb-salmon, var(--nb-black));
 }
 
 .logo-letter:nth-child(7n + 2) {
-  --logo-color: var(--nb-orange);
+  --logo-color: var(--nb-orange, var(--nb-black));
 }
 
 .logo-letter:nth-child(7n + 3) {
-  --logo-color: var(--nb-brand-yellow);
+  --logo-color: var(--nb-brand-yellow, var(--nb-black));
 }
 
 .logo-letter:nth-child(7n + 4) {
-  --logo-color: var(--nb-lime);
+  --logo-color: var(--nb-lime, var(--nb-black));
 }
 
 .logo-letter:nth-child(7n + 5) {
-  --logo-color: var(--nb-teal);
+  --logo-color: var(--nb-teal, var(--nb-black));
 }
 
 .logo-letter:nth-child(7n + 6) {
-  --logo-color: var(--nb-duck-blue);
+  --logo-color: var(--nb-duck-blue, var(--nb-black));
 }
 
 .logo-letter:nth-child(7n) {
-  --logo-color: var(--nb-purple);
+  --logo-color: var(--nb-purple, var(--nb-black));
 }
 
 .sidebar-nav {
@@ -418,11 +444,11 @@ const logoLetters = computed(() => logoText.split(""));
   background: transparent;
   border: var(--nb-border-width) solid transparent;
   border-radius: var(--nb-radius);
-  font-family: var(--nb-font-mono);
-  font-weight: 700;
+  font-family: var(--nb-font-ui, var(--nb-font-mono));
+  font-weight: var(--nb-ui-font-weight, 700);
   font-size: 14px;
-  text-transform: uppercase;
-  letter-spacing: 0.02em;
+  text-transform: var(--nb-ui-text-transform, uppercase);
+  letter-spacing: var(--nb-ui-letter-spacing, 0.02em);
   cursor: pointer;
   transition: var(--nb-transition);
   position: relative;
@@ -434,7 +460,7 @@ const logoLetters = computed(() => logoText.split(""));
 
 .nav-item:hover {
   background: var(--nb-gray-100);
-  border-color: var(--nb-black);
+  border-color: var(--nb-border-color);
   color: var(--nb-black);
   transform: translate(var(--nb-lift-x), var(--nb-lift-y));
   box-shadow: var(--nb-shadow-sm);
@@ -442,9 +468,9 @@ const logoLetters = computed(() => logoText.split(""));
 
 .nav-item.active {
   background: var(--nb-primary);
-  border-color: var(--nb-ink);
-  color: var(--nb-ink);
-  font-weight: 900;
+  border-color: var(--nb-border-color);
+  color: var(--nb-primary-foreground, var(--nb-ink));
+  font-weight: var(--nb-ui-font-weight-strong, 900);
   box-shadow: none;
 }
 
@@ -472,15 +498,6 @@ const logoLetters = computed(() => logoText.split(""));
   background: var(--nb-gray-100);
 }
 
-.theme-type-btn {
-  width: 100%;
-  background: var(--nb-gray-100);
-}
-
-.theme-type-btn:hover {
-  background: var(--nb-surface);
-}
-
 .user-section {
   padding: var(--nb-space-sm) var(--nb-space-md);
   display: flex;
@@ -499,8 +516,8 @@ const logoLetters = computed(() => logoText.split(""));
 }
 
 .username {
-  font-family: var(--nb-font-mono);
-  font-weight: 700;
+  font-family: var(--nb-font-ui, var(--nb-font-mono));
+  font-weight: var(--nb-ui-font-weight, 700);
   font-size: 13px;
   white-space: nowrap;
 }
@@ -517,18 +534,18 @@ const logoLetters = computed(() => logoText.split(""));
   border: var(--nb-border);
   border-radius: var(--nb-radius);
   cursor: pointer;
-  font-family: var(--nb-font-mono);
-  font-weight: 700;
+  font-family: var(--nb-font-ui, var(--nb-font-mono));
+  font-weight: var(--nb-ui-font-weight, 700);
   font-size: 12px;
-  text-transform: uppercase;
-  letter-spacing: 0.02em;
+  text-transform: var(--nb-ui-text-transform, uppercase);
+  letter-spacing: var(--nb-ui-letter-spacing, 0.02em);
   transition: var(--nb-transition);
   box-shadow: none;
 }
 
 .logout-btn:hover {
   background: var(--nb-gray-100);
-  border-color: var(--nb-black);
+  border-color: var(--nb-border-color);
   transform: translate(var(--nb-lift-x), var(--nb-lift-y));
   box-shadow: var(--nb-shadow-sm);
 }
@@ -536,14 +553,6 @@ const logoLetters = computed(() => logoText.split(""));
 .logout-btn:active {
   transform: translate(0, 0);
   box-shadow: none;
-}
-
-.theme-btn {
-  background: var(--nb-gray-100);
-}
-
-.theme-btn:hover {
-  background: var(--nb-surface);
 }
 
 .collapse-btn {
