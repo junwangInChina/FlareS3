f12925370d278709948ab5bb098780d9e1990b37
wangjun <578382239@qq.com>
2025-12-26 11:47:42 +0800

    switch样式调整

--- frontend/src/components/ui/BrutalSwitch.vue
+++ frontend/src/components/ui/BrutalSwitch.vue
@@ -76,10 +76,10 @@ const toggle = () => {
 }
 
 .switch-label {
-  font-family: var(--nb-font-mono);
-  font-weight: 700;
+  font-family: var(--nb-font-ui, var(--nb-font-mono));
+  font-weight: var(--nb-ui-font-weight, 700);
   font-size: 14px;
-  text-transform: uppercase;
+  text-transform: var(--nb-ui-text-transform, uppercase);
 }
 
 .disabled {
