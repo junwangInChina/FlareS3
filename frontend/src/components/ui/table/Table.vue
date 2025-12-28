46d6449bc2affbae79500641ab1ab2c44c08d8bf
wangjun <578382239@qq.com>
2025-12-28 09:57:00 +0800

    Table组件切换器

---
+++ frontend/src/components/ui/table/Table.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from 'vue'
+import { useThemeStore } from '../../../stores/theme'
+import BrutalTable from './BrutalTable.vue'
+import ShadcnTable from './ShadcnTable.vue'
+
+const props = defineProps({
+  columns: { type: Array, default: () => [] },
+  data: { type: Array, default: () => [] },
+  loading: Boolean
+})
+
+const themeStore = useThemeStore()
+const currentComponent = computed(() => {
+  return themeStore.uiTheme === 'shadcn' ? ShadcnTable : BrutalTable
+})
+</script>
+
+<template>
+  <component :is="currentComponent" v-bind="props" />
+</template>
