b70aa4b0631a12c5d6037d2936c2139d864f3f9b
wangjun <578382239@qq.com>
2025-12-28 09:52:53 +0800

    弹窗组件切换器

---
+++ frontend/src/components/ui/modal/Modal.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue'
+import { useThemeStore } from '../../../stores/theme'
+import BrutalModal from './BrutalModal.vue'
+import ShadcnModal from './ShadcnModal.vue'
+
+const props = defineProps({
+  show: Boolean,
+  title: String,
+  width: { type: String, default: '500px' }
+})
+
+const emit = defineEmits(['update:show', 'close'])
+
+const themeStore = useThemeStore()
+const currentComponent = computed(() => {
+  return themeStore.uiTheme === 'shadcn' ? ShadcnModal : BrutalModal
+})
+</script>
+
+<template>
+  <component
+    :is="currentComponent"
+    v-bind="props"
+    @update:show="emit('update:show', $event)"
+    @close="emit('close')"
+  >
+    <slot />
+    <template v-if="$slots.footer" #footer>
+      <slot name="footer" />
+    </template>
+  </component>
+</template>
