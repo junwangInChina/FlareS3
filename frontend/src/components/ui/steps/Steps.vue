7fca7965b10e57d80beba7e06512831124902162
wangjun <578382239@qq.com>
2025-12-28 09:55:46 +0800

    Step组件切换器

---
+++ frontend/src/components/ui/steps/Steps.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from 'vue'
+import { useThemeStore } from '../../../stores/theme'
+import BrutalSteps from './BrutalSteps.vue'
+import ShadcnSteps from './ShadcnSteps.vue'
+
+const props = defineProps({
+  current: { type: Number, default: 1 },
+  steps: { type: Array, default: () => [] },
+  status: { type: String, default: 'process' }
+})
+
+const themeStore = useThemeStore()
+const currentComponent = computed(() => {
+  return themeStore.uiTheme === 'shadcn' ? ShadcnSteps : BrutalSteps
+})
+</script>
+
+<template>
+  <component :is="currentComponent" v-bind="props" />
+</template>
