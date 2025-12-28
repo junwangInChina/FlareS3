ea80c4c1514a25a17706bd564fe007f66be5105c
wangjun <578382239@qq.com>
2025-12-28 09:27:34 +0800

    FormItem组件切换器

---
+++ frontend/src/components/ui/form-item/FormItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from 'vue'
+import { useThemeStore } from '../../../stores/theme'
+import BrutalFormItem from './BrutalFormItem.vue'
+import ShadcnFormItem from './ShadcnFormItem.vue'
+
+const props = defineProps({
+  label: String
+})
+
+const themeStore = useThemeStore()
+const currentComponent = computed(() => {
+  return themeStore.uiTheme === 'shadcn' ? ShadcnFormItem : BrutalFormItem
+})
+</script>
+
+<template>
+  <component :is="currentComponent" v-bind="props">
+    <slot />
+  </component>
+</template>
