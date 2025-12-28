6e5287443042c7543007240c671fa7f4d0b2ceba
wangjun <578382239@qq.com>
2025-12-28 09:22:09 +0800

    Button组件切换器

---
+++ frontend/src/components/ui/button/Button.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { computed } from 'vue'
+import { useThemeStore } from "../../../stores/theme"
+import BrutalButton from './BrutalButton.vue'
+import ShadcnButton from './ShadcnButton.vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'primary',
+    validator: (v) => ['primary', 'secondary', 'danger', 'ghost', 'default'].includes(v)
+  },
+  size: {
+    type: String,
+    default: 'medium',
+    validator: (v) => ['small', 'medium', 'large'].includes(v)
+  },
+  block: Boolean,
+  disabled: Boolean,
+  loading: Boolean
+})
+
+const emit = defineEmits(['click'])
+
+const themeStore = useThemeStore()
+const currentComponent = computed(() => {
+  return themeStore.uiTheme === 'shadcn' ? ShadcnButton : BrutalButton
+})
+</script>
+
+<template>
+  <component
+    :is="currentComponent"
+    v-bind="props"
+    @click="emit('click', $event)"
+  >
+    <slot />
+  </component>
+</template>
