e48c3e48146878aa33e8c50705681db01dd69988
wangjun <578382239@qq.com>
2025-12-28 09:53:46 +0800

    进度组件

---
+++ frontend/src/components/ui/progress/Progress.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from 'vue'
+import { useThemeStore } from '../../../stores/theme'
+import BrutalProgress from './BrutalProgress.vue'
+import ShadcnProgress from './ShadcnProgress.vue'
+
+const props = defineProps({
+  percentage: { type: Number, default: 0 },
+  type: { type: String, default: 'line' },
+  showIndicator: { type: Boolean, default: true },
+  color: String,
+  height: { type: Number, default: 24 }
+})
+
+const themeStore = useThemeStore()
+const currentComponent = computed(() => {
+  return themeStore.uiTheme === 'shadcn' ? ShadcnProgress : BrutalProgress
+})
+</script>
+
+<template>
+  <component :is="currentComponent" v-bind="props">
+    <slot />
+  </component>
+</template>
