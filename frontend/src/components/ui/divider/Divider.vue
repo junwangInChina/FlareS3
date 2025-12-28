cbbe7815e5b460666e9dd46d143869111f57db50
wangjun <578382239@qq.com>
2025-12-28 09:26:45 +0800

    Divider组件切换器

---
+++ frontend/src/components/ui/divider/Divider.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed } from 'vue'
+import { useThemeStore } from '../../../stores/theme'
+import BrutalDivider from './BrutalDivider.vue'
+import ShadcnDivider from './ShadcnDivider.vue'
+
+const themeStore = useThemeStore()
+const currentComponent = computed(() => {
+  return themeStore.uiTheme === 'shadcn' ? ShadcnDivider : BrutalDivider
+})
+</script>
+
+<template>
+  <component :is="currentComponent" />
+</template>
