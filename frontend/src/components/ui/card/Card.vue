b31871c6ef2c19e5cdd37922efc267db928d8050
wangjun <578382239@qq.com>
2025-12-28 09:22:49 +0800

    Card组件切换器

---
+++ frontend/src/components/ui/card/Card.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { computed } from 'vue'
+import { useThemeStore } from "../../../stores/theme"
+import BrutalCard from './BrutalCard.vue'
+import ShadcnCard from './ShadcnCard.vue'
+
+const props = defineProps({
+  title: String,
+  headerBg: { type: String, default: 'var(--nb-accent)' },
+  headerColor: String
+})
+
+const themeStore = useThemeStore()
+const currentComponent = computed(() => {
+  return themeStore.uiTheme === 'shadcn' ? ShadcnCard : BrutalCard
+})
+</script>
+
+<template>
+  <component :is="currentComponent" v-bind="props">
+    <template v-if="$slots.header" #header>
+      <slot name="header" />
+    </template>
+    <template v-if="$slots['header-extra']" #header-extra>
+      <slot name="header-extra" />
+    </template>
+    <slot />
+    <template v-if="$slots.footer" #footer>
+      <slot name="footer" />
+    </template>
+  </component>
+</template>
