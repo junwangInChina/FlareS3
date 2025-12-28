fe0af77f5db9aaf83479058ac394df329f68ae1a
wangjun <578382239@qq.com>
2025-12-28 09:26:04 +0800

    Desc组件切换器

---
+++ frontend/src/components/ui/descriptions/Descriptions.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { computed } from 'vue'
+import { useThemeStore } from '../../../stores/theme'
+import BrutalDescriptions from './BrutalDescriptions.vue'
+import ShadcnDescriptions from './ShadcnDescriptions.vue'
+
+const props = defineProps({
+  items: { type: Array, default: () => [] },
+  column: { type: Number, default: 2 }
+})
+
+const themeStore = useThemeStore()
+const currentComponent = computed(() => {
+  return themeStore.uiTheme === 'shadcn' ? ShadcnDescriptions : BrutalDescriptions
+})
+</script>
+
+<template>
+  <component :is="currentComponent" v-bind="props" />
+</template>
