d3d02b4e3173f784f0912ad08c0bd991f268081a
wangjun <578382239@qq.com>
2025-12-28 09:27:22 +0800

    Shadcn主题Divider组件

---
+++ frontend/src/components/ui/divider/ShadcnDivider.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="shadcn-divider"></div>
+</template>
+
+<style scoped>
+.shadcn-divider {
+  height: 1px;
+  background-color: var(--nb-border-color);
+  margin: 16px 0;
+}
+</style>
