1d1f34396246c7cce134684aa12f959741ec6d94
wangjun <578382239@qq.com>
2025-12-28 09:26:56 +0800

    Brutal主题Divider组件

---
+++ frontend/src/components/ui/divider/BrutalDivider.vue
@@ -0,0 +1,11 @@
+<template>
+  <hr class="brutal-divider" />
+</template>
+
+<style scoped>
+.brutal-divider {
+  border: 0;
+  border-top: 3px dashed var(--nb-black);
+  margin: var(--nb-space-lg) 0;
+}
+</style>
