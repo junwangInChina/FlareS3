02810aa514293c0e12c68e986cb970ad79e5d87d
wangjun <578382239@qq.com>
2025-12-29 16:43:45 +0800

    toast切换器

--- frontend/src/components/ui/toast/Toast.vue
+++ frontend/src/components/ui/toast/Toast.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useThemeStore } from '../../../stores/theme'
 import BrutalToast from './BrutalToast.vue'
 import ShadcnToast from './ShadcnToast.vue'
@@ -8,8 +8,19 @@ const themeStore = useThemeStore()
 const currentComponent = computed(() => {
   return themeStore.uiTheme === 'shadcn' ? ShadcnToast : BrutalToast
 })
+
+const innerRef = ref(null)
+
+defineExpose({
+  add: (message, type, duration) => innerRef.value?.add?.(message, type, duration),
+  remove: (id) => innerRef.value?.remove?.(id),
+  success: (message, duration) => innerRef.value?.success?.(message, duration),
+  error: (message, duration) => innerRef.value?.error?.(message, duration),
+  warning: (message, duration) => innerRef.value?.warning?.(message, duration),
+  info: (message, duration) => innerRef.value?.info?.(message, duration)
+})
 </script>
 
 <template>
-  <component :is="currentComponent" />
+  <component :is="currentComponent" ref="innerRef" />
 </template>
