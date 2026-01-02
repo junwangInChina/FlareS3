10ec23829ac929d6db8254edcbf3ae935722b73c
wangjun <578382239@qq.com>
2026-01-02 16:16:03 +0800

    英语、基础模块

---
+++ frontend/src/locales/en-US/index.js
@@ -0,0 +1,22 @@
+import common from "./common";
+import sidebar from "./sidebar";
+import components from "./components";
+import upload from "./upload";
+import auth from "./pages/auth";
+import audit from "./pages/audit";
+import users from "./pages/users";
+import files from "./pages/files";
+import setup from "./pages/setup";
+
+export default {
+  ...common,
+  ...sidebar,
+  ...components,
+  ...upload,
+  ...auth,
+  ...audit,
+  ...users,
+  ...files,
+  ...setup,
+};
+
