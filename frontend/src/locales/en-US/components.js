79c7af660c57dea66040cf11e2105ace0756fc10
wangjun <578382239@qq.com>
2026-01-02 16:13:39 +0800

    英语、组件模块

---
+++ frontend/src/locales/en-US/components.js
@@ -0,0 +1,22 @@
+export default {
+  pagination: {
+    ariaLabel: "Pagination",
+    total: "{total} total",
+    prev: "Previous",
+    next: "Next",
+    more: "More pages",
+    perPage: "Per page",
+  },
+  table: {
+    empty: "No data",
+  },
+  input: {
+    clear: "Clear input",
+  },
+  dateRange: {
+    placeholder: "Start date - End date",
+    endDate: "End date",
+    clear: "Clear date range",
+  },
+};
+
