c587e530d9093fd5eb6161526384ad7988ede3ac
wangjun <578382239@qq.com>
2026-01-02 16:14:52 +0800

    中文、组件模块

---
+++ frontend/src/locales/zh-CN/components.js
@@ -0,0 +1,22 @@
+export default {
+  pagination: {
+    ariaLabel: "分页",
+    total: "共 {total} 条",
+    prev: "上一页",
+    next: "下一页",
+    more: "更多页",
+    perPage: "每页",
+  },
+  table: {
+    empty: "暂无数据",
+  },
+  input: {
+    clear: "清除输入",
+  },
+  dateRange: {
+    placeholder: "开始日期 - 结束日期",
+    endDate: "结束日期",
+    clear: "清除日期范围",
+  },
+};
+
