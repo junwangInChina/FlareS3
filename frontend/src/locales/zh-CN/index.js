59611f7c51301c22b9e5911ec4081067b7705a29
wangjun <578382239@qq.com>
2026-01-05 14:14:13 +0800

    中文、侧边栏

--- frontend/src/locales/zh-CN/index.js
+++ frontend/src/locales/zh-CN/index.js
@@ -1,12 +1,12 @@
-import common from "./common";
-import sidebar from "./sidebar";
-import components from "./components";
-import upload from "./upload";
-import auth from "./pages/auth";
-import audit from "./pages/audit";
-import users from "./pages/users";
-import files from "./pages/files";
-import setup from "./pages/setup";
+import common from './common'
+import sidebar from './sidebar'
+import components from './components'
+import upload from './upload'
+import auth from './pages/auth'
+import audit from './pages/audit'
+import users from './pages/users'
+import files from './pages/files'
+import setup from './pages/setup'
 
 export default {
   ...common,
@@ -18,5 +18,4 @@ export default {
   ...users,
   ...files,
   ...setup,
-};
-
+}
