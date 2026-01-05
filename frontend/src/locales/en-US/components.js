a16329e4abf9564108328acaee02e1b0e222779b
wangjun <578382239@qq.com>
2026-01-05 14:12:01 +0800

    多语言，组件

--- frontend/src/locales/en-US/components.js
+++ frontend/src/locales/en-US/components.js
@@ -1,22 +1,21 @@
 export default {
   pagination: {
-    ariaLabel: "Pagination",
-    total: "{total} total",
-    prev: "Previous",
-    next: "Next",
-    more: "More pages",
-    perPage: "Per page",
+    ariaLabel: 'Pagination',
+    total: '{total} total',
+    prev: 'Previous',
+    next: 'Next',
+    more: 'More pages',
+    perPage: 'Per page',
   },
   table: {
-    empty: "No data",
+    empty: 'No data',
   },
   input: {
-    clear: "Clear input",
+    clear: 'Clear input',
   },
   dateRange: {
-    placeholder: "Start date - End date",
-    endDate: "End date",
-    clear: "Clear date range",
+    placeholder: 'Start date - End date',
+    endDate: 'End date',
+    clear: 'Clear date range',
   },
-};
-
+}
