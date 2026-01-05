7cdbd47f6727715b12b65a00421034d6b1140d08
wangjun <578382239@qq.com>
2026-01-05 14:14:40 +0800

    中文、授权

--- frontend/src/locales/zh-CN/pages/auth.js
+++ frontend/src/locales/zh-CN/pages/auth.js
@@ -1,18 +1,18 @@
 export default {
   auth: {
-    subtitle: "轻量级临时文件分享",
-    username: "用户名",
-    password: "密码",
-    usernamePlaceholder: "请输入用户名",
-    passwordPlaceholder: "请输入密码",
-    login: "登录",
-    adminTip: "管理员账号通过环境变量初始化",
-    usernamePasswordRequired: "请输入用户名和密码",
-    loginFailed: "登录失败",
+    subtitle: '轻量级临时文件分享',
+    username: '用户名',
+    password: '密码',
+    usernamePlaceholder: '请输入用户名',
+    passwordPlaceholder: '请输入密码',
+    login: '登录',
+    adminTip: '管理员账号通过环境变量初始化',
+    usernamePasswordRequired: '请输入用户名和密码',
+    loginFailed: '登录失败',
     errors: {
-      userNotFound: "账号不存在",
-      passwordIncorrect: "密码错误",
-      userDisabled: "账号被禁用",
+      userNotFound: '账号不存在',
+      passwordIncorrect: '密码错误',
+      userDisabled: '账号被禁用',
     },
   },
-};
+}
