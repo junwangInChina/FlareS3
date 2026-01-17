717e4770dc10cfb75b1cbd39ec2b7fffeab888ce
wangjun <578382239@qq.com>
2026-01-17 09:51:13 +0800

    短链

--- worker/src/routes/shortlink.ts
+++ worker/src/routes/shortlink.ts
@@ -2,6 +2,7 @@ import type { Env } from '../config/env'
 import { getUser } from './utils'
 import { jsonResponse } from './utils'
 import { viewTextShare } from './textShares'
+import { tryViewTextOneTimeShare } from './textOneTimeShares'
 
 export async function shortlink(request: Request, env: Env, code: string): Promise<Response> {
   if (!code) return jsonResponse({ error: '短码不能为空' }, 400)
@@ -11,6 +12,8 @@ export async function shortlink(request: Request, env: Env, code: string): Promi
     .bind(code)
     .first()
   if (!file) {
+    const oneTime = await tryViewTextOneTimeShare(request, env, code)
+    if (oneTime) return oneTime
     return viewTextShare(request, env, code)
   }
   if (Number(file.require_login) === 1) {
