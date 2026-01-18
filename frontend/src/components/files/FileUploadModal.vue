598fbab95736902ef0498dc45a7ca48011e8e156
wangjun <578382239@qq.com>
2026-01-18 10:18:42 +0800

    文件上传组件

---
+++ frontend/src/components/files/FileUploadModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <Modal
+    :show="show"
+    :title="t('files.modals.uploadTitle')"
+    width="760px"
+    @update:show="handleUpdateShow"
+  >
+    <UploadPanel v-if="show" @uploaded="handleUploaded" />
+  </Modal>
+</template>
+
+<script setup>
+import { useI18n } from 'vue-i18n'
+import Modal from '../ui/modal/Modal.vue'
+import UploadPanel from '../upload/UploadPanel.vue'
+
+defineProps({
+  show: Boolean,
+})
+
+const emit = defineEmits(['update:show', 'uploaded'])
+
+const { t } = useI18n({ useScope: 'global' })
+
+const handleUpdateShow = (value) => {
+  emit('update:show', value)
+}
+
+const handleUploaded = (payload) => {
+  emit('uploaded', payload)
+}
+</script>
+
