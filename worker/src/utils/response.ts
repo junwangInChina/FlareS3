9a28444868ef192d0fdc6765f7dde9c9e9b05362
wangjun <578382239@qq.com>
2026-01-25 20:38:33 +0800

    返回

---
+++ worker/src/utils/response.ts
@@ -0,0 +1,11 @@
+export function jsonResponse(
+  data: unknown,
+  status = 200,
+  extraHeaders?: HeadersInit
+): Response {
+  const headers = new Headers(extraHeaders)
+  if (!headers.has('Content-Type')) {
+    headers.set('Content-Type', 'application/json')
+  }
+  return new Response(JSON.stringify(data), { status, headers })
+}
