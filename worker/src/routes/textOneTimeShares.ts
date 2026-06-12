26e13d9cb8636b406880832932924b921fb1dfef
wangjun <578382239@qq.com>
2026-06-12 13:31:42 +0800

    文档分享

--- worker/src/routes/textOneTimeShares.ts
+++ worker/src/routes/textOneTimeShares.ts
@@ -3,6 +3,7 @@ import { getUser, jsonResponse } from './utils'
 import { renderConfirmPage, renderContentPage, renderMessagePage } from './textShares'
 import { generateRandomCode } from '../utils/random'
 import { SHARE_SHORT_CODE_LENGTH } from '../utils/codePolicy'
+import { formatDateTimeLocal } from '../services/shareFormatting'
 
 type LoadTextAuthResult =
   | {
@@ -11,21 +12,6 @@ type LoadTextAuthResult =
       ownerId: string
     }
   | { response: Response }
-
-function formatDateTimeLocal(isoString: string | null): string {
-  if (!isoString) return ''
-  const date = new Date(isoString)
-  const time = date.getTime()
-  if (Number.isNaN(time)) return ''
-
-  const pad = (n: number) => String(n).padStart(2, '0')
-  const y = date.getFullYear()
-  const m = pad(date.getMonth() + 1)
-  const d = pad(date.getDate())
-  const hh = pad(date.getHours())
-  const mm = pad(date.getMinutes())
-  return `${y}-${m}-${d} ${hh}:${mm}`
-}
 
 function getD1Changes(result: unknown): number {
   const metaChanges = Number((result as any)?.meta?.changes ?? 0)
--- worker/src/routes/textShares.ts
+++ worker/src/routes/textShares.ts
@@ -19,6 +19,7 @@ import {
 import { generateRandomCode } from '../utils/random'
 import { SHARE_SHORT_CODE_LENGTH } from '../utils/codePolicy'
 import { buildPage, escapeHtml, htmlResponse } from './sharePage'
+import { formatDateTimeLocal } from '../services/shareFormatting'
 
 type LoadTextAuthResult =
   | { response: Response }
@@ -324,21 +325,6 @@ export async function deleteTextShare(
   }
 
   return jsonResponse({ success: true, deleted: true })
-}
-
-function formatDateTimeLocal(isoString: string | null): string {
-  if (!isoString) return ''
-  const date = new Date(isoString)
-  const time = date.getTime()
-  if (Number.isNaN(time)) return ''
-
-  const pad = (n: number) => String(n).padStart(2, '0')
-  const y = date.getFullYear()
-  const m = pad(date.getMonth() + 1)
-  const d = pad(date.getDate())
-  const hh = pad(date.getHours())
-  const mm = pad(date.getMinutes())
-  return `${y}-${m}-${d} ${hh}:${mm}`
 }
 
 async function resolveShareRecord(env: Env, code: string): Promise<ResolveShareRecordResult> {
